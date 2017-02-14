7a3cac461d7a7f55476a41f64016c2860a32a27b
David Hong <davidhong@David-Mac-pro15.local>
2017-02-14 23:48:14 +0900

    add conversation.py

---
+++ 02-data-type-1.py
@@ -0,0 +1,9 @@
+
+print(1 + 1)
+print(1.5 + 2.1)
+print("hello")
+
+print(int(4/2))
+print("1")
+print(int("1"))
+print(int("1.5"))
---
+++ conversation.py
@@ -0,0 +1,5 @@
+# 화면에 출력
+print(1+2)
+# name = input("what's your name ? ")
+# print("hello, " + name)
+print ("hey "+"hello")
