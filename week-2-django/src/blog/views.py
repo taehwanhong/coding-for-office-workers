0d4b95a6fb16035d74f91deda053105fde3e5426
davidhong <taehwan.hong@gmail.com>
2017-02-27 00:34:48 +0900

    update all

--- week-2-django/src/blog/views.py
+++ week-2-django/src/blog/views.py
@@ -1,7 +1,12 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
-
+# from random import randint
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    # random_numner = randint(1,10)
+    # return HttpResponse("Hello, world. You're at the index. {}".format(random_numner))
+    name = "taehwan"
+    return render(request, "index.html", {"name" : name})
+
+
 # Create your views here.
