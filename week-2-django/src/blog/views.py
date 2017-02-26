855b2ab9a50197fabc6a01241f9bc59b302ad080
davidhong <taehwan.hong@gmail.com>
2017-02-27 01:23:46 +0900

    second weeks lecture on going

--- week-2-django/src/blog/views.py
+++ week-2-django/src/blog/views.py
@@ -1,12 +1,26 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 # from random import randint
+from .models import Article
+
+# Create your views here.
+
 
 def index(request):
     # random_numner = randint(1,10)
     # return HttpResponse("Hello, world. You're at the index. {}".format(random_numner))
-    name = "taehwan"
-    return render(request, "index.html", {"name" : name})
+    # name = "taehwan"
+    # return render(request, "index.html", {"name" : name})
 
+    article_list = Article.objects.all()
+    # Article.objects.create(
+    #     title = "hello",
+    #     contents="this is test",
+    #     view_count = 0,
+    # )
+    ctx = {
+        "article_list" : article_list,
+    }
 
-# Create your views here.
+    return render(request, "index.html", ctx)
+
