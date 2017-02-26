7c19c8d6dddfad031273e708993cc959f3b6de3c
davidhong <taehwan.hong@gmail.com>
2017-02-26 16:23:49 +0900

    update all

--- week-2-django/src/blog/views.py
+++ week-2-django/src/blog/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+    return HttpResponse("Hello, world. You're at the index.")
 # Create your views here.
